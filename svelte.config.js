1c008c516ea113c3f88da492fcf421b7f9edfc2e
Noah Salvi <noahsalvi@me.com>
2021-08-07 23:45:10 +0200

    fix prod flag

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,8 @@ import WindiCSS from "vite-plugin-windicss";
 import adapter from "@sveltejs/adapter-node";
 import fs from "fs";
 
+const productionMode = process.env.NODE_ENV === "production";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -15,7 +17,7 @@ const config = {
     vite: {
       plugins: [WindiCSS.default()],
       server: {
-        https: process.env.PRODUCTION
+        https: productionMode
           ? {
               key: fs.readFileSync(
                 "/etc/letsencrypt/live/noahsalvi.ch/fullchain.pem"
