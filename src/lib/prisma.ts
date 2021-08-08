46a38b751337609a61829786fbbd908a0f03eebc
Noah Salvi <noahsalvi@me.com>
2021-08-08 23:11:37 +0200

    fix prisma not working in adapter mode and try out new deploy script

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,4 @@
-import * as Prisma from "@prisma/client";
-
-const PrismaClient = Prisma.PrismaClient
-  ? Prisma.PrismaClient
-  : (Prisma as any).default.PrismaClient;
-
-const prisma: Prisma.PrismaClient = new PrismaClient();
-
+import Prisma, * as PrismaScope from "@prisma/client";
+const PrismaClient = Prisma?.PrismaClient || PrismaScope?.PrismaClient;
+const prisma = new PrismaClient();
 export default prisma;
