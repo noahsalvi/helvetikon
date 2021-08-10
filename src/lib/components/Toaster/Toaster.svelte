8de8a248501d40b7ae15b4a02f78de67d50c4bd9
Noah Salvi <noahsalvi@me.com>
2021-08-10 18:54:19 +0200

    make padding larger for toasts

--- src/lib/components/Toaster/Toaster.svelte
+++ src/lib/components/Toaster/Toaster.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <section class="fixed w-full z-100">
-  <div class="mx-auto mt-4 px-10 flex flex-col items-center gap-2">
+  <div class="mx-auto mt-4 px-6 flex flex-col items-center gap-2">
     {#each $toasts as toast (toast)}
       <Toast on:destroy={() => removeToast(toast)} {...toast} />
     {/each}
