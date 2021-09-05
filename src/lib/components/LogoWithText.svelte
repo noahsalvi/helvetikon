b7f334d7fa1f1630d1b99194ccd9321357aef2a7
Noah Salvi <noahsalvi@me.com>
2021-09-05 23:20:11 +0200

    edit logo with text pos

--- src/lib/components/LogoWithText.svelte
+++ src/lib/components/LogoWithText.svelte
@@ -6,9 +6,9 @@
   export let logo = true;
 </script>
 
-<a {href} class="flex justify-center items-baseline {$$props.class}">
+<a {href} class="flex justify-center items-end {$$props.class}">
   {#if logo}
-    <Logo class="transform translate-y-1 h-10 mr-3" />
+    <Logo class="h-10 mr-3" />
   {/if}
-  <h1 class="text-3xl font-serif">{config.appName}</h1>
+  <h1 class="text-3xl font-serif leading-none">{config.appName}</h1>
 </a>
