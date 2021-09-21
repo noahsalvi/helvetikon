102d611e32d2c4be81a554b629eee93c4806d04a
Noah Salvi <noahsalvi@me.com>
2021-09-21 20:15:52 +0200

    add theme-color and placeholder pages

---
+++ src/routes/faq.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Nav from "$lib/components/Nav.svelte";
+</script>
+
+<Nav />
+
+<main class="container">
+  <div class="h-5" />
+  <h1 class="text-4xl text-coal font-bold">FAQ</h1>
+
+  Todo
+</main>
+
+<svelte:head>
+  <title>FAQ | Helvetikon</title>
+</svelte:head>
---
+++ src/routes/impressum.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Nav from "$lib/components/Nav.svelte";
+</script>
+
+<Nav />
+
+<main class="container">
+  <div class="h-5" />
+  <h1 class="text-4xl text-coal font-bold">Impressum</h1>
+
+  Todo
+</main>
+
+<svelte:head>
+  <title>Impressum | Helvetikon</title>
+</svelte:head>
